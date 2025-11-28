11e86df1f3f3af7b41b6a7f205d8edef9234f929
Markobobr <markthegoat1945@gmail.com>
2025-11-28 06:16:40 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,3 @@
+const C="rb-v1",F=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
